d436e015af333c63d8c1d67d3e7f0f82ee51ce50
DESKTOP-FGJ7EA4\ErikSommer <eriksommer1@gmail.com>
2018-10-25 06:25:57 -0500

    add to Amazon Review

--- Hot_Key_Scripts/AmazonReviewScraper.py
+++ Hot_Key_Scripts/AmazonReviewScraper.py
@@ -1,3 +1,17 @@
 import sys
+import scrapy
 
 url = sys.argv[1]
+
+class AmazonReview(scrapy.Spider):
+    name = "Amazon Review"
+    start_urls = [url]
+
+    def parse(self, response):
+        file = open("C:\\Users\\Erik\\Documents\\Scripts\\AHK_Script\\Hot_Key_Scripts\\Review.txt", "w")
+        SET_SELECTOR = '[data-hook="review-body"]'
+
+        for review in response.css(SET_SELECTOR):
+            file.write(review)
+
+        file.close()
