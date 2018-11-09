895c958b6b4d0488dc7eb74f6155cc9632e2f4b1
DESKTOP-FGJ7EA4\ErikSommer <eriksommer1@gmail.com>
2018-11-09 06:49:12 -0600

    amazon review updates

--- Hot_Key_Scripts/AmazonReviewScraper.py
+++ Hot_Key_Scripts/AmazonReviewScraper.py
@@ -1,17 +1,24 @@
 import sys
-import scrapy
+import requests
+from bs4 import BeautifulSoup
+
+def get_review_text(html):
+    review_text = ''
+    for tag in html.findAll('span', {'class': 'a-size-base review-text'}):
+        review_text = review_text + ''.join(tag.findAll(text=True))
+    return review_text
+
+def get_page_html(web_page):
+    page = requests.get(web_page)
+    soup = BeautifulSoup(page.text)
+    return soup
+
+def write_to_file(text):
+    file = open("C:\\Users\\Erik\\Documents\\Scripts\\AHK_Script\\Hot_Key_Scripts\\Review.txt", "w")
+    file.write(review_text)
+    file.close()
 
 url = sys.argv[1]
-
-class AmazonReview(scrapy.Spider):
-    name = "Amazon Review"
-    start_urls = [url]
-
-    def parse(self, response):
-        file = open("C:\\Users\\Erik\\Documents\\Scripts\\AHK_Script\\Hot_Key_Scripts\\Review.txt", "w")
-        SET_SELECTOR = '[data-hook="review-body"]'
-
-        for review in response.css(SET_SELECTOR):
-            file.write(review)
-
-        file.close()
+html = get_page_html(url)
+review_text = get_review_text(html)
+write_to_file(review_text)
